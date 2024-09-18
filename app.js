760716421f0423915dc675418991df0c9b5d232a
CleinerMQ <106049886+CleinerMQ@users.noreply.github.com>
2024-09-17 23:53:28 -0500

    Update app.js
    Yy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Añade interactividad al mover el mouse para rotar el ramo
-document.addEventListener('mousemove', (event) => {
-    const bouquet = document.querySelector('.bouquet');
-    const xRotation = (event.clientY / window.innerHeight) * 360;
-    const yRotation = (event.clientX / window.innerWidth) * 360;
-    bouquet.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
-});
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
