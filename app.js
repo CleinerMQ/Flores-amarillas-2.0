3b449bbfe69e74013c6ed7dbc261ba6b48892bfb
CleinerMQ <106049886+CleinerMQ@users.noreply.github.com>
2024-09-17 23:00:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿// Aquí puedes agregar más interacción si es necesario.
+document.addEventListener('mousemove', (event) => {
+    const container = document.querySelector('.flower-container');
+    const xRotation = (event.clientY / window.innerHeight) * 360;
+    const yRotation = (event.clientX / window.innerWidth) * 360;
+    container.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+});
+
